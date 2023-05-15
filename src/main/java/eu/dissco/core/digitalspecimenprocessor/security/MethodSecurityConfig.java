0517e4f36993edde02dfbeab0975686759eff71d
southeo <soulaine.theo@gmail.com>
2023-05-15 17:05:27 +0200

    Trivyignore

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/security/MethodSecurityConfig.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/security/MethodSecurityConfig.java
@@ -1,16 +1,16 @@
 package eu.dissco.core.digitalspecimenprocessor.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+@EnableMethodSecurity
+public class MethodSecurityConfig {
 
-  @Override
+  @Bean
   protected MethodSecurityExpressionHandler createExpressionHandler() {
     return new DefaultMethodSecurityExpressionHandler();
   }
