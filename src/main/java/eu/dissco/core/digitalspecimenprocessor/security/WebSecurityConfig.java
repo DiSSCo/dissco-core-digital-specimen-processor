088ee5e6b4a184ae93da128fcf0f81139a8a5ff0
southeo <soulaine.theo@gmail.com>
2023-05-19 14:31:59 +0200

    Actuator endpoint security

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/security/WebSecurityConfig.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/security/WebSecurityConfig.java
@@ -1,9 +1,10 @@
 package eu.dissco.core.digitalspecimenprocessor.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,19 +13,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig{
+public class WebSecurityConfig {
 
-  private final JwtAuthConverter jwtAuthConverter;
+    private final JwtAuthConverter jwtAuthConverter;
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests()
-        .requestMatchers(HttpMethod.GET, "**").permitAll()
-        .anyRequest().authenticated();
-    http.oauth2ResourceServer()
-        .jwt()
-        .jwtAuthenticationConverter(jwtAuthConverter);
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+                .anyRequest().authenticated();
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
 }
