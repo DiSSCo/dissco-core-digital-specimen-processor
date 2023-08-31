c8c62cdf0722fb267a66a604f08b955b6fd41819
southeo <soulaine.theo@gmail.com>
2023-08-31 17:01:20 +0200

    test coverage

--- src/test/java/eu/dissco/core/digitalspecimenprocessor/service/FdoRecordServiceTest.java
+++ src/test/java/eu/dissco/core/digitalspecimenprocessor/service/FdoRecordServiceTest.java
@@ -103,6 +103,22 @@ class FdoRecordServiceTest {
   }
 
   @Test
+  void testGenRequestMinimalCombined() throws Exception {
+    // Given
+    var specimen = new DigitalSpecimen(PHYSICAL_SPECIMEN_ID, TYPE,
+        givenDigitalSpecimenAttributesMinimal(), givenDigitalSpecimenAttributesMinimal());
+    ((ObjectNode)specimen.attributes()).put("ods:physicalSpecimenIdType", "combined");
+    var expected = new ArrayList<>(
+        List.of(givenHandleRequestMin()));
+
+    // When
+    var response = builder.buildPostHandleRequest(List.of(specimen));
+
+    // Then
+    assertThat(response).isEqualTo(expected);
+  }
+
+  @Test
   void testRollbackUpdate() throws Exception {
     var specimen = new DigitalSpecimen(PHYSICAL_SPECIMEN_ID, TYPE,
         givenDigitalSpecimenAttributesMinimal(), givenDigitalSpecimenAttributesMinimal());
