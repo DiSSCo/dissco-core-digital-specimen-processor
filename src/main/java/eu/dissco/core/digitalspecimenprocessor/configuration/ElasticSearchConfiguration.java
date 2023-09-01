f724c8db04c12b7780d328696ef8cb7ce96faedd
Sam Leeflang <sam.leeflang@naturalis.nl>
2023-09-01 11:49:54 +0200

    Add elastic credentials to calls + handle exception

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/configuration/ElasticSearchConfiguration.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/configuration/ElasticSearchConfiguration.java
@@ -8,6 +8,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.dissco.core.digitalspecimenprocessor.property.ElasticSearchProperties;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,8 +24,13 @@ public class ElasticSearchConfiguration {
 
   @Bean
   public ElasticsearchClient elasticsearchClient() {
-    RestClient restClient = RestClient.builder(
-        new HttpHost(properties.getHostname(), properties.getPort())).build();
+    var credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(AuthScope.ANY,
+        new UsernamePasswordCredentials(properties.getUsername(), properties.getPassword()));
+    RestClient restClient = RestClient.builder(new HttpHost(properties.getHostname(),
+            properties.getPort()))
+        .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+            .setDefaultCredentialsProvider(credentialsProvider)).build();
     ElasticsearchTransport transport = new RestClientTransport(restClient,
         new JacksonJsonpMapper(mapper));
     return new ElasticsearchClient(transport);
