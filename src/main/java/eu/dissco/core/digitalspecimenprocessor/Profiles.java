2b315ba33425ffa1449633da4290d5f34327d9c5
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-04-25 08:32:51 +0200

    Remove + renameProfile

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/Profiles.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/Profiles.java
@@ -1,7 +1,7 @@
 package eu.dissco.core.digitalspecimenprocessor;
 
 public class Profiles {
-  public static final String RABBIT_MQ = "rabbitMQ";
+  public static final String RABBIT_MQ = "rabbitmq";
   public static final String WEB = "web";
 
   private Profiles(){}
