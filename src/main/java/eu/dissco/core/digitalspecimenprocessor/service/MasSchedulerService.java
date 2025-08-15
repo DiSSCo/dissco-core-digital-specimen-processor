23879661502d89b683dc687f8a78a7c9ac42fdd4
Soulaine <soulaine.theo@gmail.com>
2025-08-15 14:17:49 +0200

    Get all ids

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/service/MasSchedulerService.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/service/MasSchedulerService.java
@@ -10,7 +10,9 @@ import eu.dissco.core.digitalspecimenprocessor.domain.media.MediaProcessResult;
 import eu.dissco.core.digitalspecimenprocessor.domain.specimen.DigitalSpecimenEvent;
 import eu.dissco.core.digitalspecimenprocessor.domain.specimen.DigitalSpecimenRecord;
 import eu.dissco.core.digitalspecimenprocessor.property.ApplicationProperties;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -34,10 +36,7 @@ public class MasSchedulerService {
   public void scheduleMasSpecimenFromEvent(Set<DigitalSpecimenEvent> specimenEvents,
       List<DigitalSpecimenRecord> digitalSpecimenRecords,
       SpecimenProcessResult specimenProcessResult) {
-    var idMap = digitalSpecimenRecords.stream().collect(Collectors.toMap(
-        specimen -> specimen.digitalSpecimenWrapper().physicalSpecimenID(),
-        DigitalSpecimenRecord::id
-    ));
+    var idMap = getIdMap(digitalSpecimenRecords, specimenProcessResult);
     var newPhysicalSpecimenIds = specimenProcessResult.newSpecimens().stream()
         .map(event -> event.digitalSpecimenWrapper().physicalSpecimenID()).collect(
             Collectors.toSet());
@@ -60,6 +59,27 @@ public class MasSchedulerService {
         }
       }
     }
+  }
+
+  private static Map<String, String> getIdMap(List<DigitalSpecimenRecord> digitalSpecimenRecords,
+      SpecimenProcessResult specimenProcessResult) {
+    var idMapNew = digitalSpecimenRecords.stream().collect(Collectors.toMap(
+        specimen -> specimen.digitalSpecimenWrapper().physicalSpecimenID(),
+        DigitalSpecimenRecord::id
+    ));
+    var idMapUpdate = specimenProcessResult.changedSpecimens().stream().collect(Collectors.toMap(
+        updatedSpecimen -> updatedSpecimen.currentSpecimen().digitalSpecimenWrapper()
+            .physicalSpecimenID(),
+        updatedSpecimen -> updatedSpecimen.currentSpecimen().id()
+    ));
+    var idMapEqual = specimenProcessResult.equalSpecimens().stream().collect(Collectors.toMap(
+        equalSpecimen -> equalSpecimen.digitalSpecimenWrapper().physicalSpecimenID(),
+        DigitalSpecimenRecord::id
+    ));
+    var idMap = new HashMap<>(idMapNew);
+    idMap.putAll(idMapUpdate);
+    idMap.putAll(idMapEqual);
+    return idMap;
   }
 
   public void scheduleMasMediaFromEvent(Set<DigitalMediaEvent> mediaEvents,
