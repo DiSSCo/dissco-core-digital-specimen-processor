7c63d4f74483ab362b5db2a566ba3559cdd3f662
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-10-28 09:23:44 +0100

    Upgrade Elastic to 9.2.0

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/configuration/ElasticSearchConfiguration.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/configuration/ElasticSearchConfiguration.java
@@ -2,16 +2,15 @@ package eu.dissco.core.digitalspecimenprocessor.configuration;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.dissco.core.digitalspecimenprocessor.property.ElasticSearchProperties;
+import java.util.Base64;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.elasticsearch.client.RestClient;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.message.BasicHeader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,14 +23,14 @@ public class ElasticSearchConfiguration {
 
   @Bean
   public ElasticsearchClient elasticsearchClient() {
-    var credentialsProvider = new BasicCredentialsProvider();
-    credentialsProvider.setCredentials(AuthScope.ANY,
-        new UsernamePasswordCredentials(properties.getUsername(), properties.getPassword()));
-    RestClient restClient = RestClient.builder(new HttpHost(properties.getHostname(),
-            properties.getPort()))
-        .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
-            .setDefaultCredentialsProvider(credentialsProvider)).build();
-    ElasticsearchTransport transport = new RestClientTransport(restClient,
+    var creds = Base64.getEncoder()
+        .encodeToString((properties.getUsername() + ":" + properties.getPassword()).getBytes());
+    var restClient = Rest5Client
+        .builder(new HttpHost(properties.getHostname(), properties.getPort()))
+        .setDefaultHeaders(new Header[]{
+            new BasicHeader("Authorization", "Basic " + creds)
+        });
+    var transport = new Rest5ClientTransport(restClient.build(),
         new JacksonJsonpMapper(mapper));
     return new ElasticsearchClient(transport);
   }
