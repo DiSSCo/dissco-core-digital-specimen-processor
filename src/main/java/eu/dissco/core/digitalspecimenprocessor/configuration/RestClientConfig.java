3fb1353776baeb851166268ccf69b15244815f0d
Soulaine <soulaine.theo@gmail.com>
2026-04-13 16:01:56 +0200

    restclient error handling

--- src/main/java/eu/dissco/core/digitalspecimenprocessor/configuration/RestClientConfig.java
+++ src/main/java/eu/dissco/core/digitalspecimenprocessor/configuration/RestClientConfig.java
@@ -1,26 +1,30 @@
 package eu.dissco.core.digitalspecimenprocessor.configuration;
 
+import static lombok.Lombok.sneakyThrow;
+
 import eu.dissco.core.digitalspecimenprocessor.client.PidClient;
+import eu.dissco.core.digitalspecimenprocessor.exception.PidException;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-import reactor.netty.http.client.HttpClient;
 
 @Configuration
-public class WebClientConfig {
+@Slf4j
+public class RestClientConfig {
 
 	@Value("${doi.endpoint}")
 	private String doiEndpoint;
@@ -40,22 +44,24 @@ public class WebClientConfig {
 
 	@Bean
 	public PidClient doiClient(OAuth2AuthorizedClientManager authorizedClientManager) {
-		var errorResponseFilter = ExchangeFilterFunction
-			.ofResponseProcessor(WebClientErrorHandling::exchangeFilterResponseProcessor);
-		// Set up Oauth2
-		var oauth2Client = new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-		oauth2Client.setDefaultClientRegistrationId("dissco");
-		// Build web client
-		var webClient = WebClient.builder()
-			.apply(oauth2Client.oauth2Configuration())
-			.filter(errorResponseFilter)
-			.clientConnector(new ReactorClientHttpConnector(HttpClient.create().followRedirect(true)))
+		var interceptor = new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+		interceptor.setClientRegistrationIdResolver(request -> "dissco");
+		// Create RestClient
+		var restClient = RestClient.builder()
+			.requestInterceptor(interceptor)
+			// On status error, log the response and throw a PidException
+			.defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
+				var body = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
+				log.error("An error has occurred with the DOI service. Status: {}, response: {}",
+						response.getStatusCode(), body);
+				throw sneakyThrow(new PidException("An error has occurred creating the PID"));
+			})
 			.baseUrl(doiEndpoint)
 			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 			.build();
 		// Create factory for client proxies
 		var proxyFactory = HttpServiceProxyFactory.builder()
-			.exchangeAdapter(WebClientAdapter.create(webClient))
+			.exchangeAdapter(RestClientAdapter.create(restClient))
 			.build();
 		// Create client proxy
 		return proxyFactory.createClient(PidClient.class);
